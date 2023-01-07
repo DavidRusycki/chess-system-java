8e710c7f4d5b2c5ff44e146b010e02d4cb92ad2f
davidrusycki <davidrusyck@gmail.com>
2023-01-07 14:22:10 -0300

    correções para promoção

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,8 +41,13 @@ public class Program {
 				
 				if (chessMatch.getPromoted() != null) {
 					System.out.println("Entre com a peça para promoção (B/N/R/Q): ");
-					String type = sc.nextLine();
-					chessMatch.replacePromotedPiece(type);
+					String type = sc.nextLine().toUpperCase();
+					
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.println("Valor inválido, Entre com a peça para promoção (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
+					chessMatch.replacePromotedPiece(type);						
 				}
 			}
 			catch(ChessException e) {
